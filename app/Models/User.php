4b52ff61ce12296c649e221cfa8b067a94673db0
15217613 <15217613@uagro.mx>
2025-09-09 17:43:40 -0600

    ADD: Agregado Laravel Permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,18 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Notifications\Notifiable;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-use Laravel\Passport\Contracts\OAuthenticatable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Laravel\Passport\Contracts\OAuthenticatable;
 
 class User extends Authenticatable implements OAuthenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, HasRoles;
 
     /**
      * The attributes that are mass assignable.
