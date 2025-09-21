c5c84555e2032482d6c05ac43fa0604fc220ca71
15217613 <15217613@uagro.mx>
2025-09-21 11:27:29 -0600

    ADD: Agregadas rutas de prueba

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TestMailController;
+use App\Http\Controllers\TestReportController;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('test-mail')->group(function () {
+    Route::get('/blade', [TestMailController::class, 'blade']);
+    Route::get('/markdown', [TestMailController::class, 'markdown']);
+    Route::get('/simple', [TestMailController::class, 'simple']);
+    Route::get('/with-attachment', [TestMailController::class, 'withAttachment']);
+    Route::get('/with-pdf', [TestMailController::class, 'withPdf']);
+});
+
+Route::prefix('test-pdf')->group(function () {
+    Route::get('/generate', [TestReportController::class, 'generatePdf']);
 });
